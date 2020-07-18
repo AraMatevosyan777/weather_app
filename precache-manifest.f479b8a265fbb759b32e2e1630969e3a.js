02c685dbb1c7b6bf7482afb2b9c76ca965311cd8
Aro Matevosyan <AroMatevosyan777@gmil.com>
2020-07-18 10:12:20 +0400

    Updates

---
+++ precache-manifest.f479b8a265fbb759b32e2e1630969e3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64ea2bd90a33d31d41c840aecac11f17",
+    "url": "/weather_app/index.html"
+  },
+  {
+    "revision": "e2d3b142e4a3670c8cf3",
+    "url": "/weather_app/static/css/main.a7864f93.chunk.css"
+  },
+  {
+    "revision": "f2d6d390418425d20ec5",
+    "url": "/weather_app/static/js/2.8ee83e2e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/weather_app/static/js/2.8ee83e2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2d3b142e4a3670c8cf3",
+    "url": "/weather_app/static/js/main.afefa41f.chunk.js"
+  },
+  {
+    "revision": "f47d7b424f76f8774c7f",
+    "url": "/weather_app/static/js/runtime-main.c28633ec.js"
+  },
+  {
+    "revision": "5b18094ea6426297afc7cd21681735f7",
+    "url": "/weather_app/static/media/loader.5b18094e.svg"
+  }
+]);
